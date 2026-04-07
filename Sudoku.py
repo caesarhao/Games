070a59346146801edd6efc84be3756331555a9f7
Jungle Schmidt <caesarhao@gmail.com>
2026-04-07 16:32:27 +0200

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,3 +1,20 @@
+class Sudoku:
+    def __init__(self):
+        self.table = [[0 for i in range(9)] for j in range(9)]
+    def initGame(self):
+        pass
+    def runGame(self):
+        pass
+
+class SudokuTk(Sudoku):
+    def __init__(self):
+        pass
+    def initGame(self):
+        pass
+    def runGame(self):
+        pass
 
 if __name__ == '__main__':
-    pass
+    game = SudokuTk()
+    game.initGame()
+    game.runGame()
