f9f9db25308167def9e44496ad9d94aa4e3698a6
Jungle Schmidt <caesarhao@gmail.com>
2026-04-14 15:09:41 +0200

    Update Rubic.py

--- Rubic.py
+++ Rubic.py
@@ -1,5 +1,21 @@
 # to learn Rubik Cube
 
+class Rubic:
+    def __init__(self, n = 3):
+        self.N = n
+    def numCubies(self) -> int:
+        return self.N ** 3 - (self.N - 1) ** 3
+    def numCornerCubies(self) -> int:
+        return 8
+    def numCenterCubies(self) -> int:
+        if self.N % 2 == 0:
+            return 0
+        else:
+            return 6
+    def numEdgeCubies(self) -> int:
+        return self.numCubies() - self.numCornerCubies() - self.numCenterCubies()
+
 if __name__ == "__main__":
-    pass
+    rub = Rubic(3)
+    print(rub)
     
